888cffcef2103509308beabafe5c2442778daca8
Vitor Correa <vitorcorrea18@gmail.com>
2021-10-18 16:19:22 -0400

    concluidos desafios 1 a 6 #VQV

--- src/challenges.js
+++ src/challenges.js
@@ -16,27 +16,53 @@ function calcArea(base, height) {
 };
 
 // Desafio 3
-function splitSentence() {
+function splitSentence(sentence) {
   // seu código aqui
+  let splitedSent = sentence.split(' ');
+  return splitedSent; 
+  // Resolvi este desafio consultando o método split no W3schools.
+  // fonte: https://www.w3schools.com/jsref/jsref_split.asp
 }
 
 // Desafio 4
-function concatName() {
+function concatName(entrada) {
   // seu código aqui
-}
+  let lastName = entrada.length - 1;
+  let name = entrada[lastName] + ', ' + entrada[0];
+  return name;
+};
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
   // seu código aqui
-}
+  let winPoints = wins * 3;
+  let tiePoints = ties;
+  let totalPoints = winPoints + tiePoints;
+  return totalPoints;
+};
 
 // Desafio 6
-function highestCount() {
+function highestCount(num) {
   // seu código aqui
+  let highN = num[0];
+  let count = 0;
+  //primeiro for define o numero mais alto.
+  for (i = 0; i < num.length; i +=1) {
+    if (num[i] > highN) {
+      highN = num[i];
+    }
+  }
+  //segundo for conta quantas vezes o numero é repetido.
+  for (i = 0; i < num.length; i += 1) {
+    if (num[i] === highN) {
+      count++;
+    }
+  }
+  return count;
 }
 
 // Desafio 7
-function catAndMouse() {
+function catAndMouse(cat1, cat2, mouse) {
   // seu código aqui
 }
 
@@ -53,7 +79,7 @@ function decode() {
   // seu código aqui
 }
 
-/*module.exports = {
+module.exports = {
   calcArea,
   catAndMouse,
   compareTrue,
@@ -64,7 +90,8 @@ function decode() {
   footballPoints,
   highestCount,
   splitSentence,
-};*/
+};
+
 
 
 
