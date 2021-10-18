c022656c78ccb16483a79550a4677e085e4951df
Vitor Correa <vitorcorrea18@gmail.com>
2021-10-18 14:28:02 -0400

    Iniciando projeto : primeiro desafio concluido

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,14 @@
 // Desafio 1
-function compareTrue() {
+function compareTrue(a, b) {
   // seu código aqui
-}
+  if (a === true && b === true) {
+    return true;
+    console.log(true);
+  } else {
+    return false;
+    console.log(false);
+  }
+};
 
 // Desafio 2
 function calcArea() {
